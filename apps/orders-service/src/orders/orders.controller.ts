de6528240ce6d2be4e19fe9f1ce549d619efa5ff
Van Minh <vovanminhv23@gmail.com>
2026-01-04 01:13:35 +0700

    feat(microservices): integrate Redis transport for order processing

--- apps/orders-service/src/orders/orders.controller.ts
+++ apps/orders-service/src/orders/orders.controller.ts
@@ -1,11 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy, MessagePattern } from '@nestjs/microservices';
+import { MICROSERVICES } from 'src/constants';
 
 @Controller('orders')
 export class OrdersController {
+  constructor(
+    @Inject(MICROSERVICES.PRODUCT_REDIS_CLIENT)
+    private productRedisClient: ClientProxy,
+  ) {}
   @MessagePattern('create_order')
   createOrder(order: any) {
-    console.log({ message: "Order received on the Order Microservice", order });
-    return { message: 'Order Created', order };
+    console.log({ message: 'Order received on the Order Microservice', order });
+    this.productRedisClient.emit('order.created', order);
+
+    // return { message: 'Order Created', order };
+    return this.productRedisClient.send('get_product', {});
   }
 }
--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -3,15 +3,26 @@ import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      port: 4002,
-    },
-  });
-
-  await app.listen();
-  console.log('Product Microservice is running on port 4002');
+  const tcpMicroservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.TCP,
+      options: {
+        port: 4002,
+      },
+    });
+  const redisMicroservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.REDIS,
+      options: {
+        host: 'localhost',
+        port: 6379,
+      },
+    });
+  await Promise.all([tcpMicroservice.listen(), redisMicroservice.listen()]);
+  // await app.listen();
+  console.log(
+    'Product Microservice is running on port 4002, listen to redis events',
+  );
 }
 
 bootstrap();
